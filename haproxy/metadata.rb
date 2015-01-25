7292c780148872a799499a13e56229f028a71405
Mieke <mieke.mocke@gmail.com>
2015-01-25 07:38:40 -0800

    Update metadata.rb

--- haproxy/metadata.rb
+++ haproxy/metadata.rb
@@ -4,3 +4,5 @@ license           "Apache 2.0"
 description       "Installs and configures haproxy"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.4"
+
+supports 'amazon'
