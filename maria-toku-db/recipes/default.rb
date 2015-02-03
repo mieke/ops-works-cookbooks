a02f14c78a2003156f1c8407979c282633921972
Mieke <mieke.mocke@gmail.com>
2015-02-03 08:12:30 -0800

    Delete default.rb

--- maria-toku-db/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-#apt_package resource is used to manage packages for the Debian and Ubuntu platforms.
-apt_package "mariadb-tokudb-engine" do
-  version node[:mariadb-tokudb-engine_version]
-  action :install
-end
-
