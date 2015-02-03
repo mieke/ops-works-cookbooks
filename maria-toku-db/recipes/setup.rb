407dad41923668d31f034d001ade2a8120d03722
Mieke <mieke.mocke@gmail.com>
2015-02-03 08:50:46 -0800

    Update setup.rb

--- maria-toku-db/recipes/setup.rb
+++ maria-toku-db/recipes/setup.rb
@@ -1,5 +1,5 @@
 #apt_package resource is used to manage packages for the Debian and Ubuntu platforms.
 apt_package "apt-get install mariadb-server" do
-  version node[:mariadb-tokudb-engine_version]
+  version node[:mariadb_version]
   action :install
 end
