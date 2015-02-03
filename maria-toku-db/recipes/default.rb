06525d1d58866851819830bfbfe31c475f809b81
Mieke <mieke.mocke@gmail.com>
2015-02-02 17:00:32 -0800

    Update default.rb

--- maria-toku-db/recipes/default.rb
+++ maria-toku-db/recipes/default.rb
@@ -1,4 +1,6 @@
-package mariadb-tokudb-engine
+#apt_package resource is used to manage packages for the Debian and Ubuntu platforms.
+apt_package "mariadb-tokudb-engine" do
   version node[:mariadb-tokudb-engine_version]
   action :install
 end
+
