802c9b97b2e032ab9a59823adf955747db2d95b3
Mieke <mieke.mocke@gmail.com>
2015-02-03 08:43:06 -0800

    Create meta.rb

---
+++ maria-toku-db/meta.rb
@@ -0,0 +1,6 @@
+name             "maria-toku-db"
+description      "Installs MariaDB and TokuDB"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer       "OneCloud Systems"
+
+recipe "maria-toku-db::setup","Install MariaDB with TokuDB engine"
