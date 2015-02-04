46e2ce3a1194d883fc3de9d80492cd525566df1c
Mieke <mieke.mocke@gmail.com>
2015-02-04 15:14:41 -0800

    Delete server_debian.rb

--- maria-toku-db/attributes/server_debian.rb
+++
@@ -1,9 +0,0 @@
-
-case node['platform_family']
-when 'debian'
-
-  default['mariadb']['data_dir'] = '/var/lib/mysql'
-
-  default['mariadb']['server']['packages'] = %w(mariadb-server apparmor-utils)
-
-end
