d8520087477919e10461c9d78549886d28ed853c
Mieke <mieke.mocke@gmail.com>
2015-02-04 09:03:58 -0800

    Update setup.rb

--- maria-toku-db/recipes/setup.rb
+++ maria-toku-db/recipes/setup.rb
@@ -1,27 +1,4 @@
 #----
-# Set up preseeding data for debian packages
-#---
-directory '/var/cache/local/preseeding' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  recursive true
-end
-
-template '/var/cache/local/preseeding/mysql-server.seed' do
-  source 'mysql-server.seed.erb'
-  owner 'root'
-  group 'root'
-  mode '0600'
-  notifies :run, 'execute[preseed mysql-server]', :immediately
-end
-
-execute 'preseed mysql-server' do
-  command '/usr/bin/debconf-set-selections /var/cache/local/preseeding/mysql-server.seed'
-  action  :nothing
-end
-
-#----
 # Install software
 # apt_package resource is used to manage packages for the Debian and Ubuntu platforms.
 #----
