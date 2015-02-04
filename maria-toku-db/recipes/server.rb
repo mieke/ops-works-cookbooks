aeacb5d2fdb77bb43ff6d4544d6a2500ff7bd3cc
Mieke <mieke.mocke@gmail.com>
2015-02-04 15:32:42 -0800

    Update server.rb

--- maria-toku-db/recipes/server.rb
+++ maria-toku-db/recipes/server.rb
@@ -1,18 +1,6 @@
 #----
 # Install software
 #----
-node['maria-toku-db']['server']['packages'].each do |name|
-  package name do
-    action :install
-  end
-end
-
-node['maria-toku-db']['server']['directories'].each do |_key, value|
-  directory value do
-    owner     'mysql'
-    group     'mysql'
-    mode      '0775'
-    action    :create
-    recursive true
-  end
+apt_package "mariadb-server-5.5" do
+  action :install
 end
