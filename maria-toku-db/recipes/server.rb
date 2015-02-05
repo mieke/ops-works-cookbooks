df01b4ab2acc915392ec728704016fbf2369006f
Mieke <mieke.mocke@gmail.com>
2015-02-05 14:34:37 -0800

    Update server.rb

--- maria-toku-db/recipes/server.rb
+++ maria-toku-db/recipes/server.rb
@@ -3,6 +3,10 @@
 #----
 #include_recipe "apt"
 
-apt_package "mariadb-server-5.5" do
+#apt_package "mariadb-server-5.5" do
+  #action :install
+#end
+
+package 'mariadb-server' do
   action :install
 end
