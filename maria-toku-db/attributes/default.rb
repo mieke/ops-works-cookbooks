d6e369e2329ede46ee47361e212295a482fecd3f
Mieke <mieke.mocke@gmail.com>
2015-02-04 20:37:27 -0800

    Update default.rb

--- maria-toku-db/attributes/default.rb
+++ maria-toku-db/attributes/default.rb
@@ -1,4 +1,4 @@
-default['maria-toku-db']['version'] = '5.5'
-default['maria-toku-db']['disable_repo'] = false
-default['maria-toku-db']['oqgraph_install_file']= "/tmp/install_oqgraph.sql"
+#default['maria-toku-db']['version'] = '5.5'
+#default['maria-toku-db']['disable_repo'] = false
+#default['maria-toku-db']['oqgraph_install_file']= "/tmp/install_oqgraph.sql"
 
