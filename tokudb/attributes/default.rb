09bba8dbd85e79aea61e755ff44db846200809b9
Mieke <mieke.mocke@gmail.com>
2015-02-10 10:25:54 -0800

    Update default.rb

--- tokudb/attributes/default.rb
+++ tokudb/attributes/default.rb
@@ -1,5 +1,6 @@
 #default['tokudb']['url'] = 'http://www.tokutek.com/download.php?download_file=mysql-5.5.41-tokudb-7.5.5-linux-x86_64.tar.gz'
-default['tokudb']['url'] = 'http://www.tokutek.com/tokudb-for-mysql/tokudb-community-edition-download/?file=mysql-5.5.41-tokudb-7.5.5-linux-x86_64.tar.gz'
+#default['tokudb']['url'] = 'http://www.tokutek.com/tokudb-for-mysql/tokudb-community-edition-download/?file=mysql-5.5.41-tokudb-7.5.5-linux-x86_64.tar.gz'
+default['tokudb']['url'] = 'https://s3-us-west-2.amazonaws.com/mieke-tars/mysql-5.5.41-tokudb-7.5.5-linux-x86_64.tar.gz'
 default['tokudb']['install_dir'] = '/opt/tokutek/mysql'
 default['tokudb']['data_dir'] = '/var/lib/mysql'
 default['tokudb']['bin_log_path'] = '/var/lib/mysql/binlog/mysql-bin'
