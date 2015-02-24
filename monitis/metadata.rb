0b18751861a0ed58555981ee8cc8d7de6810fc8d
Mieke <mieke.mocke@gmail.com>
2015-02-24 08:36:05 -0800

    Update metadata.rb

--- monitis/metadata.rb
+++ monitis/metadata.rb
@@ -6,6 +6,7 @@ description       "Installs and configures monitis monitoring agent"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.1"
 recipe 		  "monitis", "Installs and configures Monitis Agent"
+recipe "monitis::install-jmx-agent", "Installs the JMX agent."
 
 %w{ ubuntu debian redhat centos fedora opensuse }.each do |os|
   supports os
