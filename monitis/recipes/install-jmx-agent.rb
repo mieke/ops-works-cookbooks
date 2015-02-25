a84756ed37da80d6b127e673f9f928ce386e7d8c
Mieke <mieke.mocke@gmail.com>
2015-02-25 05:53:18 -0800

    Update install-jmx-agent.rb

--- monitis/recipes/install-jmx-agent.rb
+++ monitis/recipes/install-jmx-agent.rb
@@ -9,13 +9,15 @@ else
   #action :nothing
 #end
 
+
 #Use the remote_file resource to transfer a file from a remote location using file specificity.
-remote_file "#{node['MONITIS']['JMX']['INSTALLDIR']}/" do
+#remote_file "#{node['MONITIS']['JMX']['INSTALLDIR']}/" do
+remote_file "/tmp/mon_jmx_agent.war" do
   source "#{node['MONITIS']['JMX']['WAR']}"
   #owner 'root'
   #group 'root'
   #mode '755'
   # notifies :restart, resources(:service => 'tomcat'), :immediately
-end
+#end
 
 end
