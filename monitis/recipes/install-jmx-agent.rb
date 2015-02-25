e568eb6197df77addb4d294639f48a8310727cc6
Mieke <mieke.mocke@gmail.com>
2015-02-24 16:13:10 -0800

    Update install-jmx-agent.rb

--- monitis/recipes/install-jmx-agent.rb
+++ monitis/recipes/install-jmx-agent.rb
@@ -1,13 +1,13 @@
 # Recipe specific to installing the Monitis JMS Agent
 
-#if File.exists?("#{node[:MONITIS][:JMX][:INSTALLDIR]}/mon_jmx_agent.war")
-  #self.msg("Monitis JMX Agent already installed")
-#else
+if File.exists?("#{node[:MONITIS][:JMX][:INSTALLDIR]}/mon_jmx_agent.war")
+  self.msg("Monitis JMX Agent already installed")
+else
 
-service 'tomcat' do
-  supports :restart => true, :status => true, :reload => true
-  action :nothing
-end
+#service 'tomcat' do
+  #supports :restart => true, :status => true, :reload => true
+  #action :nothing
+#end
 
 #Use the remote_file resource to transfer a file from a remote location using file specificity.
 remote_file "Download mon_jmx_agent.war" do
@@ -15,4 +15,6 @@ remote_file "Download mon_jmx_agent.war" do
   path "#{node['MONITIS']['JMX']['INSTALLDIR']}"
   backup false
   # notifies :restart, resources(:service => 'tomcat'), :immediately
+end
+
 end
